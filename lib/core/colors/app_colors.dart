47950a818581b23834d175aba5e7261e99aee9ab
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-05-24 04:39:46 +0100

    create class for color and text

---
+++ lib/core/colors/app_colors.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class AppColors{
+  static const Color primaryColor = Color(0xff070707);
+  static const Color starkWhite = Color(0xffffffff);
+  static const Color fadedWhite = Color(0xaff1ffff);
+
+
+
+}
