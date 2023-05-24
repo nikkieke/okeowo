ab2cd0612bd3ecd5318147ed4963068936fa8220
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-05-24 20:53:22 +0100

    update app colors

--- lib/core/colors/app_colors.dart
+++ lib/core/colors/app_colors.dart
@@ -2,8 +2,13 @@ import 'package:flutter/material.dart';
 
 class AppColors{
   static const Color primaryColor = Color(0xff070707);
+  static const Color leadBlack = Color(0xff000000);
+  static const Color lightBlack = Color(0xff667085);
+  static const Color inkBlue = Color(0xff344054);
+  static const Color ashGrey = Color(0xffB2BEB5);
   static const Color starkWhite = Color(0xffffffff);
   static const Color fadedWhite = Color(0xaff1ffff);
+  static const Color leadGrey = Color(0xffF4F4F4);
 
 
 
