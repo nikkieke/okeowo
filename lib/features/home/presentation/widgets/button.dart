6aaab006a1e43b8f40b5bb016e6cd291b35b5f5b
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-05-24 20:54:58 +0100

    add button widget

---
+++ lib/features/home/presentation/widgets/button.dart
@@ -0,0 +1,33 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+
+import '../../../../core/colors/app_colors.dart';
+import '../../../../widgets/app_text.dart';
+
+class Button extends StatelessWidget {
+  const Button({
+    super.key,
+    required this.text
+  });
+
+  final String text;
+
+  @override
+  Widget build(BuildContext context) {
+    return Container(
+      alignment: Alignment.center,
+      height: 48.h,
+      width: double.maxFinite,
+      decoration: BoxDecoration(
+        color: AppColors.primaryColor,
+        borderRadius: BorderRadius.all(Radius.circular(8.r)),
+      ),
+      child: AppText(
+        text: text,
+        color: AppColors.starkWhite,
+        size: 16.sp,
+        fontWeight: FontWeight.w600,
+      ),
+    );
+  }
+}
