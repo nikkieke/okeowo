0bf626f736e5ac8f089043d0bedd9cb2252e015f
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-05-24 21:00:05 +0100

    update home page with its various widgets

--- lib/features/home/presentation/screens/home.dart
+++ lib/features/home/presentation/screens/home.dart
@@ -1,9 +1,15 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_screenutil/flutter_screenutil.dart';
 import 'package:flutter_svg/svg.dart';
-import 'package:google_fonts/google_fonts.dart';
+import 'package:go_router/go_router.dart';
 import 'package:okeowo/core/colors/app_colors.dart';
 import 'package:okeowo/widgets/app_text.dart';
+
+import '../../../../core/routes/routes.dart';
+import '../widgets/quick_action_items.dart';
+import '../widgets/transaction_list.dart';
+import '../widgets/voucher_button.dart';
+import '../widgets/card_widget.dart';
 
 class HomeScreen extends StatefulWidget {
   const HomeScreen({Key? key}) : super(key: key);
@@ -22,8 +28,9 @@ class _HomeScreenState extends State<HomeScreen> {
           padding: const EdgeInsets.only(bottom: 16, top: 20, left: 16,right: 16),
           child: Column(
             mainAxisAlignment: MainAxisAlignment.start,
+            crossAxisAlignment: CrossAxisAlignment.start,
             children: [
-                Row(
+              Row(
                   children: [
                     SvgPicture.asset(
                         "assets/icons/avatar.svg",
@@ -38,63 +45,38 @@ class _HomeScreenState extends State<HomeScreen> {
                   ],
                 ),
               SizedBox(height: 16.h,),
-              Container(
-                height: 126.h,
-                width: double.maxFinite,
-                padding: EdgeInsets.only(top: 22.r, bottom: 16.r, left: 16.r, right: 16.r),
-                decoration: BoxDecoration(
-                  color: AppColors.primaryColor,
-                  borderRadius: BorderRadius.all(Radius.circular(8.r))
-                ),
-                child: Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    Column(
-                      crossAxisAlignment: CrossAxisAlignment.start,
-                      children: [
-                        AppText(
-                          text: "MTOKEN WALLET",
-                          color: AppColors.fadedWhite,
-                          size: 15.sp,
-                        ),
-                        SizedBox(
-                          height: 19.h,
-                        ),
-                        Row(
-
-                          children: [
-                            AppText(
-                              text: "₦",
-                              color: AppColors.fadedWhite,
-                              size: 12.sp,
-                              fontFamily: GoogleFonts.inter().fontFamily,
-                            ),
-                            AppText(
-                              text: " 450,900.78",
-                              color: AppColors.starkWhite,
-                              size: 18.sp,
-                              fontWeight: FontWeight.w600,
-                            ),
-                          ],
-                        ),
-                        SizedBox(
-                          height: 19.h,
-                        ),
-                        AppText(
-                          text: "Eniola Michael",
-                          color: AppColors.fadedWhite,
-                          size: 15.sp,
-                        ),
-                      ],
-                    ),
-                    SvgPicture.asset(
-                      "assets/icons/eye.svg",
-                      width: 20.w,
-                    ),
-                  ],
-                ),
+              const CardWidget(),
+              SizedBox(height: 12.h,),
+              RawMaterialButton(
+                  onPressed: (){
+                    context.push("${Routes.HOME}/${Routes.GENERATE_VOUCHER}");
+                  },
+                  child: const VoucherButton()),
+              SizedBox(height: 24.h,),
+              AppText(
+                text: "Quick Actions",
+                fontWeight: FontWeight.w700,
+                size: 16.sp,
               ),
+              SizedBox(height: 12.h,),
+              const Row(
+                mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                children: [
+                  QuickActionsItem(text: 'Redeem', iconPath: "assets/icons/redeem.svg",),
+                  QuickActionsItem(text: 'Fund Wallet', iconPath: "assets/icons/fund_wallet.svg",),
+                  QuickActionsItem(text: 'Withdraw', iconPath: "assets/icons/withdraw.svg",),
+                  QuickActionsItem(text: 'Scan Code', iconPath: "assets/icons/scan_code.svg",),
+                ],
+              ),
+              SizedBox(height: 24.h,),
+              AppText(
+                text: "Transactions",
+                fontWeight: FontWeight.w700,
+                size: 16.sp,
+              ),
+              SizedBox(height: 12.h,),
+              const TransactionList()
+
             ],
           ),
         ),
@@ -102,3 +84,12 @@ class _HomeScreenState extends State<HomeScreen> {
     );
   }
 }
+
+
+
+
+
+
+
+
+
