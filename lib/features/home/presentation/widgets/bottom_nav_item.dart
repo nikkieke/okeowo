2b535a1d834f9e075d9f9211c9c48d5a2f86a03c
nikkieke <95222620+nikkieke@users.noreply.github.com>
2023-05-24 04:40:12 +0100

    add bottom nav widget

---
+++ lib/features/home/presentation/widgets/bottom_nav_item.dart
@@ -0,0 +1,71 @@
+import 'package:flutter/material.dart';
+import 'package:flutter_screenutil/flutter_screenutil.dart';
+import 'package:okeowo/core/colors/app_colors.dart';
+import 'package:okeowo/widgets/app_text.dart';
+
+class BottomNavItem extends StatefulWidget {
+  final VoidCallback onTap;
+  final String? iconPath;
+  final String? iconSelectedPath;
+  final String? title;
+  final String initialLocation;
+  bool? isCurrent;
+
+
+  BottomNavItem({Key? key,
+    required this.onTap,
+    this.iconPath,
+    this.iconSelectedPath,
+    this.title,
+    this.isCurrent = false,
+    required this.initialLocation
+
+  }) : super(key: key);
+
+  @override
+  State<BottomNavItem> createState() => _BottomNavItemState();
+}
+
+class _BottomNavItemState extends State<BottomNavItem> {
+  @override
+  Widget build(BuildContext context) {
+    return TextButton(
+        onPressed: widget.onTap,
+        style: TextButton.styleFrom(
+          shape: const RoundedRectangleBorder(
+            borderRadius: BorderRadius.only(
+              topLeft: Radius.circular(10),
+              topRight: Radius.circular(10)
+            )
+          )
+        ),
+        child: Padding(
+          padding: EdgeInsets.zero,
+          child: Column(
+            mainAxisAlignment: MainAxisAlignment.center,
+            children: [
+              Image.asset(
+                width: 24,
+                widget.isCurrent == false?
+                    widget.iconPath!:
+                    widget.iconSelectedPath!,
+              ),
+
+              const SizedBox(
+                height: 1.5,
+              ),
+              AppText(
+                  text: widget.title!,
+                  fontWeight: FontWeight.w400,
+                  size: 12.5.sp,
+                  color: widget.isCurrent == false?
+                  AppColors.primaryColor.withOpacity(0.5):
+                      AppColors.primaryColor
+              ),
+
+            ],
+          ),
+        )
+    );
+  }
+}
